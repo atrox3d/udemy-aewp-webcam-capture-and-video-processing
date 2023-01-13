7483d43fc00b08f6fb864a47ebb0302ed0696a29
atrox3d <atrox3d@gmail.com>
2023-01-13 07:11:30 +0100

    102. Solution: Extract Frame at Specific Video Timestamp

---
+++ extract-frame-timestamp.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy
+from pathlib import Path
+
+import image_helpers
+
+# get video
+video_path = 'video.mp4'
+video = cv2.VideoCapture(video_path)
+
+# set/create images path
+images = Path('images/')
+images.mkdir(exist_ok=True)
+
+# get video stats
+fps = video.get(cv2.CAP_PROP_FPS)
+frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+print(f'{frame_count}, {fps=}')
+
+# convert timestamp to seconds
+timestamp = '00:00:01.88'
+hours, minutes, seconds = [float(n) for n in timestamp.split(':')]
+total_seconds = hours * 3600 + minutes * 60 + seconds
+frame_nr = total_seconds * fps
+print(f'{timestamp=}, {hours=}, {minutes=}, {seconds=}, {total_seconds=}, {frame_nr=}')
+
+# set current frame
+video.set(cv2.CAP_PROP_POS_FRAMES, frame_nr)
+
+# get current frame and resize it
+success, frame = video.read()
+frame = image_helpers.resize(frame, 50)
+
+# create window title
+height, width, _ = frame.shape
+title = f'{timestamp}, {frame_nr} @{width}x{height}'
+
+# show current frame in window
+cv2.imshow(title, frame)
+cv2.waitKey(0)
---
+++ image_helpers.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+import cv2
+
+
+def resize(original_image, scale_percentage=None, width=None, height=None):
+    """
+    resize an image to a % of the original or to specific dimwnsions
+
+    :param scale_percentage: percentage to which scale image
+    :param width: width if scale_percentage is None
+    :param height: height if scale_percentage is None
+
+    :return: resized image
+    """
+    print(f'{original_image.shape=}')
+    original_height, original_width, _ = original_image.shape
+
+    if scale_percentage:
+        w = int(original_width * scale_percentage / 100)
+        h = int(original_height * scale_percentage / 100)
+    elif width and height:
+        w = width
+        h = height
+    else:
+        raise ValueError('Either scale_percentage or width and height must be valorized!')
+
+    # invert width and height
+    dsize = w, h
+    print(f'{dsize=}')
+
+    resized_image = cv2.resize(original_image, dsize)
+    print(f'{resized_image.shape=}')
+
+    return resized_image
+
+
+def resize_imagefile(original_path, resized_path=None, scale_percentage=None, width=None, height=None):
+    """
+    resize an image file to a % of the original or to specific dimensions
+
+    :param original_path: path to original image
+    :param resized_path: path to resized image, if None expects width and height
+    :param scale_percentage: percentage to which scale image
+    :param width: width if scale_percentage is None
+    :param height: height if scale_percentage is None
+
+    :return: None
+    """
+    original_image = cv2.imread(original_path)
+    resized_image = resize(original_image, scale_percentage, width, height)
+
+    if not resized_path:
+        path = Path(original_path)
+        height, width, _ = original_image.shape
+        name = path.stem
+        ext = path.suffix
+        filename = f'{name}-{width}x{height}{ext}'
+        resized_path = path.with_name(filename).__str__()
+    cv2.imwrite(resized_path, resized_image)
+
+
+def get_points_from_rectangle(rectangle):
+    """
+    extracts 2 point tuples from rectangle
+    :param rectangle:
+    :return: 2 point tuples
+    """
+    x1, y1, x2, y2 = rectangle
+    pt1 = (x1, y1)
+    pt2 = (x2, y2)
+    return pt1, pt2
